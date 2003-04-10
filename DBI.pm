315f72b4b5b13cd75fee6289a4e84562d4ae8814
Netdot <netdot-devel@osl.uoregon.edu>
2003-04-10 23:28:27 +0000

    added the rest of the classes and completed the relationships between all of them

--- DBI.pm
+++ DBI.pm
@@ -25,193 +25,285 @@ __PACKAGE__->
 package Address;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('Address');
-__PACKAGE__->columns(All => qw /id street1 street2 poBox city state zip country info /);
+__PACKAGE__->table( 'Address' );
+__PACKAGE__->columns( All => qw /id street1 street2 poBox city state zip country info / );
 
-__PACKAGE__->has_many('vendors', 'Vendor' => 'address');
-__PACKAGE__->has_many('sites', 'Site' => 'address');
-__PACKAGE__->has_many('persons', 'Person' => 'address');
+__PACKAGE__->has_many( 'vendors', 'Vendor' => 'address');
+__PACKAGE__->has_many( 'sites', 'Site' => 'address');
+__PACKAGE__->has_many( 'persons', 'Person' => 'address');
+__PACKAGE__->has_many( 'peers', 'Peer' => 'address' );
+__PACKAGE__->has_many( 'customers', 'Customer' => 'address' );
 
 
 ######################################################################
 package Availability;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('Availability');
-__PACKAGE__->columns(All => /id name info/);
+__PACKAGE__->table( 'Availability' );
+__PACKAGE__->columns( All => /id name info/ );
 
-__PACKAGE__->has_many('customers', 'Customer' => 'availability');
-__PACKAGE__->has_many('sites', 'Site' => 'availability' );
-__PACKAGE__->has_many('persons', 'Person' => 'availability' );
+__PACKAGE__->has_many( 'customers', 'Customer' => 'availability');
+__PACKAGE__->has_many( 'sites', 'Site' => 'availability' );
+__PACKAGE__->has_many( 'persons', 'Person' => 'availability' );
+__PACKAGE__->has_many( 'peers', 'Peer' => 'availability' );
 
 
 ######################################################################
 package Circuit;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('Circuit');
-__PACKAGE__->columns(All => qw/ id cid vendor startSite endSite type speed installDate connection info /);
+__PACKAGE__->table( 'Circuit' );
+__PACKAGE__->columns( All => qw/id cid vendor startSite endSite type speed installDate connection status info/ );
 
-__PACKAGE__->has_a(type => 'CircuitType');
-__PACKAGE__->has_a(startSite => 'Site');
-__PACKAGE__->has_a(endSite => 'Site');
-__PACKAGE__->has_a(connection => 'Connection');
-__PACKAGE__->has_a(vendor => 'Vendor');
+__PACKAGE__->has_a( type => 'CircuitType' );
+__PACKAGE__->has_a( startSite => 'Site' );
+__PACKAGE__->has_a( endSite => 'Site' );
+__PACKAGE__->has_a( connection => 'Connection' );
+__PACKAGE__->has_a( vendor => 'Vendor' );
+__PACKAGE__->has_a( status => 'Status' );
 
 
 ######################################################################
 package CircuitType;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('CircuitType');
-__PACKAGE__->columns(All => qw/id name info /);
+__PACKAGE__->table( 'CircuitType' );
+__PACKAGE__->columns( All => qw/id name info/ );
 
-__PACKAGE__->has_many('circuits', 'Circuit' => 'type');
+__PACKAGE__->has_many( 'circuits', 'Circuit' => 'type' );
 
 
 ######################################################################
 package Connection;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('Connection');
-__PACKAGE__->columns(All => qw /id name customer startSite endSite info /);
+__PACKAGE__->table( 'Connection' );
+__PACKAGE__->columns( All => qw /id name customer startSite endSite info/ );
 
-__PACKAGE__->has_a('startSite' => 'Site');
-__PACKAGE__->has_a('endSite' => 'Site');
-__PACKAGE__->has_a('customer' => 'Customer');
-__PACKAGE__->has_many('circuits', 'Circuit' => 'connection');
+__PACKAGE__->has_a( 'startSite' => 'Site' );
+__PACKAGE__->has_a( 'endSite' => 'Site' );
+__PACKAGE__->has_a( 'customer' => 'Customer' );
+__PACKAGE__->has_many( 'circuits', 'Circuit' => 'connection' );
 
 
 ######################################################################
 package ContactInfo;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('ContactInfo');
-__PACKAGE__->columns(All => qw /id contactType contactPool person email office home cell pager emailPager fax info /);
+__PACKAGE__->table( 'ContactInfo' );
+__PACKAGE__->columns( All => qw /id contactType contactPool person email office home cell pager emailPager fax info / );
 
-__PACKAGE__->has_a(contactPool => 'ContactPool');
-__PACKAGE__->has_a(contactType => 'ContactType');
-__PACKAGE__->has_a(person => 'Person');
+__PACKAGE__->has_a( contactPool => 'ContactPool' );
+__PACKAGE__->has_a( contactType => 'ContactType' );
+__PACKAGE__->has_a( person => 'Person' );
 
 
 ######################################################################
 package ContactPool;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('ContactPool');
-__PACKAGE__->columns(All => qw /id name info /);
+__PACKAGE__->table( 'ContactPool' );
+__PACKAGE__->columns( All => qw /id name info /);
 
-__PACKAGE__->has_many('vendors', 'Vendor' => 'contactPool');
-__PACKAGE__->has_many('sites', 'Site' => 'contactPool');
-__PACKAGE__->has_many('contactinfos', 'ContactInfo' => 'contactPool');
+__PACKAGE__->has_many( 'contactinfos', 'ContactInfo' => 'contactPool' );
+__PACKAGE__->has_many( 'customers', 'Customer' => 'contactPool' );
+__PACKAGE__->has_many( 'peers', 'Peer' => 'contactPool' );
+__PACKAGE__->has_many( 'nodes', 'Node' => 'contactPool' );
+__PACKAGE__->has_many( 'vendors', 'Vendor' => 'contactPool' );
+__PACKAGE__->has_many( 'sites', 'Site' => 'contactPool' );
 
 
 ######################################################################
 package ContactType;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('ContactType');
-__PACKAGE__->columns(All => qw /id name info /);
+__PACKAGE__->table( 'ContactType' );
+__PACKAGE__->columns( All => qw /id name info / );
 
-__PACKAGE__->has_many('contactinfos', 'ContactInfo' => 'contactType');
+__PACKAGE__->has_many( 'contactinfos', 'ContactInfo' => 'contactType' );
 
 
 ######################################################################
 package Customer;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('Customer');
-__PACKAGE__->columns(All => qw /id name Hours Address ContactPool info/ );
+__PACKAGE__->table( 'Customer' );
+__PACKAGE__->columns( All => qw /id name Hours Address ContactPool info/ );
 
-__PACKAGE__->has_a(address => 'Address');
-__PACKAGE__->has_a(hours => 'Availability');
-__PACKAGE__->has_a(contactPool => 'ContactPool');
+__PACKAGE__->has_a( address => 'Address' );
+__PACKAGE__->has_a( contactPool => 'ContactPool' );
+__PACKAGE__->has_a( hours => 'Availability' );
+__PACKAGE__->has_many( 'customers', 'Customer' => 'customer' );
+__PACKAGE__->has_many( 'jncustomersites', 'JnCustomerSite' => 'customer' );
+__PACKAGE__->has_many( 'nodes', 'Node' => 'customer' );
+
 
 
 ######################################################################
 package Interface;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('Interface');
-__PACKAGE__->columns(All => qw/id nodeId hostName physAddr ifIndex ifType ifDescr ifSpeed ifStatus info/ );
+__PACKAGE__->table( 'Interface' );
+__PACKAGE__->columns( All => qw/id nodeId hostName ip physAddr ifIndex ifType ifDescr ifSpeed ifStatus info/ );
 
-__PACKAGE__->has_a(nodeID => 'Node');
-__PACKAGE__->has_many( 'interfacedeps', 'InterfaceDep' => 'parent');
-__PACKAGE__->has_many( 'interfacedeps', 'InterfaceDep' => 'child');
+__PACKAGE__->has_a( nodeID => 'Node' );
+__PACKAGE__->has_a( ip => 'Ip' );
+__PACKAGE__->has_many( 'parentinterfacedeps', 'InterfaceDep' => 'parent');
+__PACKAGE__->has_many( 'childinterfacedeps', 'InterfaceDep' => 'child');
 
 
 ######################################################################
 package InterfaceDep;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('InterfaceDep');
-__PACKAGE__->columns(All => /id parent child/ );
+__PACKAGE__->table( 'InterfaceDep' );
+__PACKAGE__->columns( All => /id parent child/ );
 
-__PACKAGE__->has_a(parent => 'Interface');
-__PACKAGE__->has_a(child => 'Interface');
+__PACKAGE__->has_a( parent => 'Interface' );
+__PACKAGE__->has_a( child => 'Interface' );
+
+
+######################################################################
+package Ip;
+use base 'Netdot::DBI';
+
+__PACKAGE__->table( 'Ip' );
+__PACKAGE__->columns( All => qw/id address mask/ );
+
+__PACKAGE__->has_many( 'interfaces', 'Interface' => 'ip' );
+
+
+######################################################################
+package JnNodeService;
+use base 'Netdot::DBI';
+
+__PACKAGE__->table( 'JnNodeService' );
+__PACKAGE__->columns( All => qw/id node service/ );
+
+__PACKAGE__->has_a( node => 'Node' );
+__PACKAGE__->has_a( service => 'Service' );
 
 
 ######################################################################
 package JnCustomerSite;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('JnCustomerSite');
-__PACKAGE__->columns(All => qw/id customer site/ );
+__PACKAGE__->table( 'JnCustomerSite' );
+__PACKAGE__->columns( All => qw/id customer site/ );
 
-__PACKAGE__->has_a(customer => 'Customer');
-__PACKAGE__->has_a(site => 'Site');
+__PACKAGE__->has_a( customer => 'Customer' );
+__PACKAGE__->has_a( site => 'Site' );
 
 
 ######################################################################
 package Model;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('Model');
-__PACKAGE__->columns(All => qw/id name vendor info/ );
+__PACKAGE__->table( 'Model' );
+__PACKAGE__->columns( All => qw/id name vendor info/ );
 
-__PACKAGE__->has_a(vendor => 'Vendor');
+__PACKAGE__->has_a( vendor => 'Vendor' );
+
+
+######################################################################
+package Node;
+use base 'Netdot::DBI';
+
+__PACKAGE__->table( 'Node' );
+__PACKAGE__->columns( All => qw/id name type sysDescription serialNumber site customer room rack dateInstalled sw_version contactPool info/ );
+
+__PACKAGE__->has_a( site => 'Site' );
+__PACKAGE__->has_a( customer => 'Customer' );
+__PACKAGE__->has_a( contactPool => 'ContactPool' );
+__PACKAGE__->has_a( type => 'NodeType' );
+__PACKAGE__->has_many( 'interfaces', 'Interface' => 'nodeID' );
+__PACKAGE__->has_many( 'jnnodeservices', 'JnNodeService' => 'node' );
+
+
+######################################################################
+package NodeType;
+use base 'Netdot::DBI';
+
+__PACKAGE__->table( 'NodeType' );
+__PACKAGE__->columns( All => qw/id name info/ );
+
+__PACKAGE__->has_many( 'nodes', 'Node' => 'type' );
+
+
+######################################################################
+package Peer;
+use base 'Netdot::Peer';
+
+__PACKAGE__->table( 'Peer' );
+__PACKAGE__->columns( All => qw/id name autSys ip address contactPool info/ );
+
+__PACKAGE__->has_a( ip => 'Ip' );
+__PACKAGE__->has_a( address => 'Address' );
+__PACKAGE__->has_a( contact_pool => 'ContactPool' );
+
 
 
 ######################################################################
 package Person;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('Person');
-__PACKAGE__->columns(All => qw /id firstName lastName position department address availability info /);
+__PACKAGE__->table( 'Person' );
+__PACKAGE__->columns( All => qw /id firstName lastName position department address availability info / );
 
-__PACKAGE__->has_a(address => 'Address');
-__PACKAGE__->has_a(availability => 'Availability');
-__PACKAGE__->has_many('contactinfos', 'ContactInfo' => 'person');
+__PACKAGE__->has_a( address => 'Address' );
+__PACKAGE__->has_a( availability => 'Availability' );
+__PACKAGE__->has_many( 'contactinfos', 'ContactInfo' => 'person' );
+
+
+######################################################################
+package Service;
+use base 'Netdot::DBI';
+
+__PACKAGE__->table( 'Service' );
+__PACKAGE__->columns( All => qw/id name/ );
+
+__PACKAGE__->has_many( 'jnnodeservices', 'JnNodeService' => 'service' );
 
 
 ######################################################################
 package Site;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('Site');
+__PACKAGE__->table( 'Site' );
+__PACKAGE__->columns( All => qw/id name availability address contactPool info /);
 
-__PACKAGE__->columns(All => qw/id name availability address contactPool info/);
+__PACKAGE__->has_a( address => 'Address' );
+__PACKAGE__->has_a( availability => 'Availability' );
+__PACKAGE__->has_a( contactPool => 'ContactPool' );
+__PACKAGE__->has_many( 'startcircuits', 'Circuit' => 'startSite' );
+__PACKAGE__->has_many( 'endcircuits', 'Circuit' => 'endSite' );
+__PACKAGE__->has_many( 'startconnections', 'Connection' => 'startSite' );
+__PACKAGE__->has_many( 'endconnections', 'Connection' => 'endSite' );
+__PACKAGE__->has_many( 'jncustomersites', 'JnCustomerSite' => 'site' );
+__PACKAGE__->has_many( 'nodes', 'Node' => 'site' );
 
-__PACKAGE__->has_a(address => 'Address');
-__PACKAGE__->has_a(availability => 'Availability');
-__PACKAGE__->has_a(contactPool => 'ContactPool');
-__PACKAGE__->has_many('startcircuits', 'Circuit' => 'startSite');
-__PACKAGE__->has_many('endcircuits', 'Circuit' => 'endSite');
-__PACKAGE__->has_many('startconnections', 'Connection' => 'startSite');
-__PACKAGE__->has_many('endconnections', 'Connection' => 'endSite');
+
+######################################################################
+package Status;
+use base 'Netdot::DBI';
+
+__PACKAGE__->table( 'Status' );
+__PACKAGE__->columns( All => qw/id name info/ );
+
+__PACKAGE__->has_many( 'circuits', 'Circuit' => 'status' );
 
 
 ######################################################################
 package Vendor;
 use base 'Netdot::DBI';
 
-__PACKAGE__->table('Vendor');
-__PACKAGE__->columns(All => qw/id name contactPool acctNumber address info/);
+__PACKAGE__->table( 'Vendor' );
+__PACKAGE__->columns( All => qw/id name contactPool acctNumber address info/ );
 
-__PACKAGE__->has_a(address => 'Address');
-__PACKAGE__->has_a(contactPool => 'ContactPool');
-__PACKAGE__->has_many('circuits', 'Circuit' => 'vendor');
-__PACKAGE__->has_many('models', 'Model' => 'vendor');
+__PACKAGE__->has_a( address => 'Address' );
+__PACKAGE__->has_a( contactPool => 'ContactPool' );
+__PACKAGE__->has_many( 'circuits', 'Circuit' => 'vendor' );
+__PACKAGE__->has_many( 'models', 'Model' => 'vendor' );
 
 
 
@@ -222,6 +314,10 @@ __PACKAGE__->has_many('models', 'Model' => 'vendor');
 
 ######################################################################
 #  $Log: DBI.pm,v $
+#  Revision 1.10  2003/04/10 23:28:27  netdot
+#  added the rest of the classes and completed the relationships between
+#  all of them
+#
 #  Revision 1.9  2003/04/10 21:51:26  netdot
 #  updated column names to new naming convention
 #
