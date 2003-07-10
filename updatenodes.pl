734e3ddcedfb1077075d99a3fbd7ae6a1dfd79e5
Netdot <netdot-devel@osl.uoregon.edu>
2003-07-10 20:54:10 +0000

    more work to complete this.  still fleshing algorithm out.

--- updatenodes.pl
+++ updatenodes.pl
@@ -6,32 +6,65 @@ use Netdot::DBI;
 use Netdot::Netviewer;
 
 my $DEBUG = 1;
+my %ifnames = ( physaddr => "ifPhysAddress",
+		ifindex => "instance",
+		iftype => "ifType",
+		ifalias => "descr",
+		ifspeed => "ifSpeed",
+		ifadminstatus => "ifAdminStatus" );
 
-my $nv = Netdot::Netviewer->new( foreground => 1 );
+my $nv = Netdot::Netviewer->new( foreground => 0 );
 my @nodes = Node->retrieve_all();
 foreach my $node ( @nodes ) {
-  print "node ", $node->name, "\n" if( $DEBUG );
+  print "!!!!!!!! node ", $node->name, "\n" if( $DEBUG );
   my @ifs = Node->interfaces();
   $, = " " if( $DEBUG );
   print @ifs if( $DEBUG );
   $nv->build_config( "device", $node->name );
   ################################################
   # get information from the device
-  if( my( %node ) = $nv->get_device( "device", $args{node} ) ) {
+  if( my( %dev ) = $nv->get_device( "device", $node->name ) ) {
     ##############################################
     # for each interface just discovered...
-    foreach my $newif ( keys %{ $node{interface} } ) {
+    foreach my $newif ( keys %{ $dev{interface} } ) {
+      ############################################
+      # set up IF state data
+      my( %iftmp, %iptmp );
+      $iftmp{node} = $node->id;
+      $iftmp{ifdescr} = $newif;
+      foreach my $dbname ( keys %ifnames ) {
+	if( $ifnames{$dbname} eq "descr" ) {
+	  if( $dev{interface}{$newif}{$ifnames{$dbname}} ne "-" ) {
+	    $iftmp{$dbname} = $dev{interface}{$newif}{$ifnames{$dbname}};
+	  }
+	} else {
+	  $iftmp{$dbname} = $dev{interface}{$newif}{$ifnames{$dbname}};
+	}
+      }
       ############################################
       # does this ifIndex already exist in the DB?
-      if( my $if = (Interface->search( ifindex => $node{interface}{$newif}{ifIndex}))[0] ) {
-	;
+      if( my $if = (Interface->search( ifindex => $dev{interface}{$newif}{ifIndex}))[0] ) {
+	update( obj => \$if, state => \%iftmp );
       } else {
-	;
+	insert( obj => "Interface", state => \%iptmp );
+      }
+      if( exists( $dev{interface}{$newif}{ipAdEntIfIndex} ) ) {
+	foreach my $newip( keys %{ $dev{interface}{$newif}{ipAdEntIfIndex}}){
+	  $iptmp{interface} = $if->id;
+	  $iptmp{address} = $newip;
+	  $iptmp{mask} = $dev{interface}{$newif}{ipAdEntIfIndex}{$newip};
+	  ########################################
+	  # does this ip already exist in the DB?
+	  if( my $ip = (Ip->search( address => $newip ))[0] ) {
+	    update( obj => \$ip, state => \%iptmp );
+	  } else {
+	    insert( obj => "Ip", state => \%iptmp );
+	}
       }
     }
     ##############################################
     # remove each interface that no longer exists
-    foreach my $nullif ( @ifs ) {
+    foreach my $nonif ( @ifs ) {
       ;
     }
   } else {
@@ -41,7 +74,42 @@ foreach my $node ( @nodes ) {
 
 
 ######################################################################
+sub update {
+  my( %argv ) = @_;
+  my($obj) = $argv{object};
+  my($state) = %{ $argv{state} };
+  my $change = 0;
+  foreach my $col ( keys %state ) {
+    if( $state{$col} ne $obj->$col ) {
+      $change = 1;
+      $obj->set( $col, $state{$col} );
+    }
+  }
+  if( $change ) {
+    $obj->update;
+  }
+}
+
+
+######################################################################
+sub insert {
+  my( %argv ) = @_;
+  my($obj) = $argv{object};
+  my($state) = %{ $argv{state} };
+  $obj->create( \%state );
+}
+
+
+######################################################################
+sub remove {
+  ;
+}
+
+######################################################################
 #  $Log: updatenodes.pl,v $
+#  Revision 1.3  2003/07/10 20:54:10  netdot
+#  more work to complete this.  still fleshing algorithm out.
+#
 #  Revision 1.2  2003/07/08 23:22:22  netdot
 #  *** empty log message ***
 #
