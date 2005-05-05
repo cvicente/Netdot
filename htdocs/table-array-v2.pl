cd5ce169ddb8ed3836a589c32c67d9ae5cf94c02
Nathan Collins <nathan@network-services.uoregon.edu>
2005-05-05 05:51:24 +0000

    A more powerful version of table-array.pl.  Seems to work very well for device.html at revision 799 (try lines 410 - 449 for example, only one major mistake is made) Will probably find that it doesn't work so well for other files but hopefully it will be easy enough to adapt.  I left table-array.pl around, not just to create more cruft, but also because it might be easier to use else where (I might have made fewer assumptions there).

---
+++ htdocs/table-array-v2.pl
@@ -0,0 +1,59 @@
+#!/usr/bin/perl -w
+
+# Nathan Collins Wed May  4 22:38:22 PDT 2005
+
+# "Enhanced" version of table-array.pl.  Adds support for
+# accumulation.  Not sure how worthwhile this actually is generally,
+# but saves a bit of time when it is needed.  Also, the code it
+# generates is little prettier than v1 did.
+
+# Probably braindead script to help convert tables into the list we
+# pass to attribute_table.mhtml.  Generated code will still require
+# some massaging but should be pretty close to what you're looking
+# for.  
+
+
+die "usage: $0 n m <file>\nto convert lines n..m of <file>\n" if ! (scalar(@ARGV) == 3);
+
+my $html = `sed -ne '$ARGV[0],$ARGV[1]p' $ARGV[2]`;
+my $toggle = 1;
+
+# Assume everything is in a table data element and that there is no
+# nesting.  Might need to consider table headers and the like.
+while ($html =~ m|<td[^>]*>(.*?)</td>|sg) {	
+    #print $1,"\n";
+    if ($toggle) {
+	print qq(push( \@field_headers, "$1" );\n);	# Assuming field headers are always simple.
+    }else{
+	my @lines = split(/\n/,$1);
+	@lines = grep { $_ !~ /^\s*$/ } @lines;		# Delete empty lines.
+
+	my $a = "";
+	if (@lines > 1) {
+	    $a = '$a .= ';	# The string form of the accumulator code.
+	}
+	foreach (@lines) { 
+	    if (/^%/) { 
+		s/^%//;	# Un Mason escape embedded perl lines.
+		s/^(\s*)printf/$1${a}sprintf/;	# Accumulate prints.
+	    }else{
+		s/^(\s*)(.*)\s*$/$1$a'$2';/;	# Trim whitespace, quote, and accumulate.
+		s/<% (.*?) %>/'.$1.'/g;	# Un Mason escape variables/method calls.
+	    }
+	    s/(\$ui->.*?\(.*)\)/$1, returnAsVar=>1\)/;	# The returnAsVar argument needs to get added to all calls to $ui's methods.
+	}
+
+	
+	my $code = join( "\n", @lines );	
+        if (@lines > 1) {	# If there are multiple lines there are probably choices and/or accumulation.
+	    $code = qq(\n&{sub{\n    my \$a = "";\n).$code."\n    \$a\n}}";	# The anon sub will return $a.
+	}else{
+	    $code =~ s/^\s*(.*)\;$/$1/;	# A loan statement shouldn't have a trailing semicolon in a call to push, and doesn't need leading whitespace.
+	}
+
+	print( "push( \@cell_data, ", 
+	       $code,
+	       " );\n" );
+    }
+    ($toggle += 1) %= 2;	# Switch between field_headers and cell_data
+}
