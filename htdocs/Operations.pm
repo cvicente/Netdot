e4dffd718031f701f227b8b1456eedcfcb7e2a20
Nathan Collins <nathan@network-services.uoregon.edu>
2006-01-20 01:24:17 +0000

    Fixed problem where all my links read "No rule for:" (due my idiotic code in view_link) which was triggered by changes in how UI::getcolumntypes worked (not sure what happened).
    Cleaned up the code in Operations.pm

--- htdocs/Operations.pm
+++ htdocs/Operations.pm
@@ -3,6 +3,8 @@ package Operations;
 use Netdot;
 use Data::Dumper;
 
+my $ui = Netdot::UI->new;
+
 # Helper methods for the operations page
 
 # Given a list of contacts find those that have availability != never
@@ -17,10 +19,6 @@ sub contactable_contacts {
     
     my @fields = qw/notify_voice notify_pager notify_email/;
 
-#     foreach my $c (@contacts) {
-# 	_debug(map { $c->$_->name } @fields);
-#     }
-    
     my @sets = map { 
 	$contact = $_; 
 	# Anon hash
@@ -39,12 +37,6 @@ sub contactable_contacts {
     _debug(@available);
 
     return @available;
-    
-# #     return grep { scalar @{ $_ } > 1 } 
-# #     map { 
-# # 	$contact = $_; 
-# # 	[$contact, grep { $contact->$_ ne "Never" } @fields] 
-# # 	} @contacts;
 }
 
 # Circuits are pretty rare: Use the generic search to find one for
@@ -69,68 +61,45 @@ sub _debug {
     #print "<pre>" . Dumper(@_) . "</pre>";
 }
 
-# Make a canonical link: one with text the name of the object pointing
-# to the view.html page for that object.
-
-# Pass the object reference.
-my $ui = Netdot::UI->new;
-sub view_link {
-    my $obj = shift;
-    my %hash = $ui->form_field(object => $obj, column => 'name', 
-			       edit => 0, linkPage => 1);
-    return $hash{'value'};
-}
-
-sub get_object {
-    my ($obj, $id, $table) = @_;
-    # Assumes no $ids are 0.  I think this is true in MySQL, not sure
-    # about other DB's
-    $obj || $id && $table->retrieve($id)
-	or die "You must supply a valid object id or reference";
-}
-
 ;{
     package Netdot::DBI;
-    # Would like to just have a package variable...
+    # Default view page for all Netdot::DBI subclasses.
     # Override this in subclasses with special view pages
     sub _viewing_page { "view.html" }
     
-    # Want to use view_link on all objects.  For this a meaningful
-    # canonical name is needed.  The ``label'' as defined in the
-    # metatable (see bin/insert-metadata) seems like a natural choice.
+    # This is a helper so you can ask an object for its label
+    # directly.  The label is defined in the metatable (see
+    # bin/insert-metadata).
     sub label {
 	my $self = shift;
 	return $ui->getobjlabel($self, ", ");
     }
     
+    # Make a canonical link: one with text the name of the object
+    # pointing to the view.html page for that object.
     sub view_link {
 	my $self = shift;
-	# Optional second arg gives viewing page.
+	# Optional second arg gives viewing page, otherwise class
+	# default is used.
 	my $page = shift || $self->_viewing_page;
-	# Doesn't work because the $self->name is a reference
-	#return '<a href="' . $self->linkPage . '?table=' . (ref $self) . '&id=' . $self->id . '">' . $self->name . '</a>';
 
-
-	# Doesn't work because it uses the id for the column passed
-	# my %hash = $ui->form_field(object => $self, column => 'name', 
-	#		       edit => 0, linkPage => $self->linkPage);
-	# return $hash{'value'} || "[ahhhhhh]";
-
-	# Used to use the column ``name'' for the name, but not all
-	# objects have that.  So, the label method was added to
-	# Netdot::DBI.
-	my %pair = $ui->form_field(object => $self, column => 'label');
-	my ($table, $id, $name) = ($self->table, 
-				   $self->id, 
-				   $pair{value});
-	return "<a href=\"${page}?table=${table}&id=${id}\"> $name </a>";
+	return "<a href=\"${page}?table=@{[ $self->table ]}&id=@{[ $self->id ]}\"> @{[ $self->label ]} </a>";
     }
+}
 
+;{
     package Device;
+    # Device has a custom view page.
     sub _viewing_page { "device.html" }
 }
 
-#sub Netdot::DBI::canon_link { return Operations::canon_link(shift); }
-
 # This is braindead
 1;
+
+# sub get_object {
+#     my ($obj, $id, $table) = @_;
+#     # Assumes no $ids are 0.  I think this is true in MySQL, not sure
+#     # about other DB's
+#     $obj || $id && $table->retrieve($id)
+# 	or die "You must supply a valid object id or reference";
+#}
