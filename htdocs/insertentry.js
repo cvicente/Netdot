bcc4141336ea55b3437c97df5de24e56a6a3a59e
Carlos Vincente <cvicente@uoregon.edu>
2006-01-28 00:06:28 +0000

    * Merging latest fixes from trunk

--- htdocs/insertentry.js
+++ htdocs/insertentry.js
@@ -11,6 +11,8 @@
 */
    var insertwind;
    function openinsertwindow(edit_args){
+      var now = new Date();
       var url = "insertentry.html?"+edit_args;
-      insertwind = window.open(url, "insertwind", "width=600,height=400,scrollbars=yes");
+      // the idea is to open a window with a unique name so that we don't override the contents of an already open window
+      insertwind = window.open(url, "insertwind"+now.getMinutes()+now.getSeconds(), "width=600,height=400,scrollbars=yes");
    }
