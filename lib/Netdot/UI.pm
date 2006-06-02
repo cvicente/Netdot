39495c29d418d4f3c885caaa8ab6664b4125957b
Carlos Vincente <cvicente@uoregon.edu>
2006-06-02 17:57:38 +0000

    Fixed bug in upgrade procedure.  Fixed typo in UI.pm

--- lib/Netdot/UI.pm
+++ lib/Netdot/UI.pm
@@ -484,7 +484,7 @@ sub form_field {
     my $id = ($o ? $o->id : "NEW");
     my $current = ($o ? $o->$column : $default);
     
-    my %linksto  = $self->getlinksto( $tableName )
+    my %linksto  = $self->getlinksto( $tableName );
     my %order    = $self->getcolumnorder( $tableName )
 	or croak "Failed to get column order for $tableName";
     my %tags     = $self->getcolumntags( $tableName )
