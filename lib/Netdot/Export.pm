bb7b0905ce67ba6553ff476c21003b6398699277
Carlos Vincente <cvicente@uoregon.edu>
2006-12-18 22:06:47 +0000

    Fixed some bugs in the routine that finds parents

--- lib/Netdot/Export.pm
+++ lib/Netdot/Export.pm
@@ -18,7 +18,7 @@ use vars qw ( @EXPORT @ISA @EXPORT_OK);
 @EXPORT    = qw( get_dependencies resolve );
 @EXPORT_OK = qw();
 
-my $DEBUG = 0;
+my $DEBUG = 1;
 
 
 1;
@@ -38,46 +38,63 @@ sub get_dependencies{
     my (%args) = @_;
     my $intobj    = $args{interface} || die "Need to pass interface object";
     my $recursive = (defined $args{recursive}) ? $args{recursive} : 1;
-    my @parents;
+    my %parents;
 
     # Get InterfaceDep objects
     my @depobjs = $intobj->parents;
+
     # Get the parent interfaces
     printf ("Looking for valid parents for %s.%s\n", 
 	    $intobj->name, $intobj->device->name->name) if $DEBUG;
-    foreach my $parent ( map { $_->parent } @depobjs ){
+
+    my @parents = map { $_->parent } @depobjs;
+    foreach my $parent ( @parents ){
 	next unless ( $parent && ref($parent) eq "Interface" );
-	# Check if it has any ips (and is managed)
-	if ( (my $ip = ($parent->ips)[0]) 
-	     && $parent->monitored 
-	     && $parent->device->monitored ){
+
+	# Check if it has any ips
+	my $ip = ($parent->ips)[0];
+
+	# Skip interface if it has no ip and there are other parents
+	if ( !$ip && scalar(@parents) != 1 ){
+	    printf ("%s.%s does not have an IP. Skipping...\n", 
+		    $parent->name, $parent->device->name->name) if $DEBUG;
+	    next;
+	}
+	# Check if interface and device are monitored
+	if ( $parent->monitored  && $parent->device->monitored ){
 	    printf ("Adding %s.%s as parent of %s.%s\n", 
 		    $parent->name, $parent->device->name->name,
 		    $intobj->name, $intobj->device->name->name) if $DEBUG;
-	    push @parents, $ip;
+	    $parents{$ip} = '';
+
 	}else{
 	    printf ("%s.%s is not a valid parent\n", 
 		    $parent->name, $parent->device->name->name) if $DEBUG;
 	    next unless ( $recursive );
+
 	    # Go recursive
 	    my @grparents;
 	    if ( @grparents = &get_dependencies(interface=>$parent, recursive=>$recursive) ){
-		push @parents, @grparents;
+		map { $parents{$_} = '' }  @grparents;
+
 	    }else{
-		# Check if Device has other interfaces which have parents
+		# No grandparents on that side.
+		# Check if parent device has other interfaces which have parents
+
 		foreach my $int ( $parent->device->interfaces ){
 		    next if $int->id == $parent->id;
-		    printf ("Trying %s.%s \n", $parent->device->name->name, $int->name) if $DEBUG;
+		    printf ("Trying %s.%s \n", $int->name, $parent->device->name->name ) if $DEBUG;
+
 		    if ( @grparents = &get_dependencies(interface=>$int, recursive=>0) ){
-			if ( my $ip = ($int->ips)[0] ){
+			if ( (my $ip = ($int->ips)[0]) && $int->monitored && $int->device->monitored ){
 			    # If this interface itself has an IP address, it should be the parent
 			    printf ("Adding %s.%s as parent of %s.%s\n", 
-				    $int->device->name->name, $int->name,
-				    $intobj->device->name->name, $intobj->name) if $DEBUG;
-			    push @parents, $ip; 
+				    $int->name, $int->device->name->name, 
+				    $intobj->name, $intobj->device->name->name) if $DEBUG;
+			    $parents{$ip} = ''; 
 			    last;
 			}else{
-			    push @parents, @grparents; 
+			    map { $parents{$_} = '' }  @grparents;
 			    last;
 			}
 		    }
@@ -85,7 +102,8 @@ sub get_dependencies{
 	    }
 	}
     }
-    return @parents if scalar @parents;
+    my @ret = keys %parents;
+    return @ret if scalar @ret;
     return;
 }
 
