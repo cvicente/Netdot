c387392ed18220f48f554ed29f8151529ea5fcb3
Carlos Vincente <cvicente@uoregon.edu>
2007-06-07 17:23:33 +0000

    Changes in bin/ directory

--- bin/DBUTIL.pm
+++ bin/DBUTIL.pm
@@ -24,22 +24,17 @@ sub dbconnect {
     my ( $db_type, $db_host, $db_port, $db_dba, $db_dba_password, $db_database ) = @_;
     my $dsn = "dbi:$db_type:";
     
-    if ($db_type =~ /^(Pg|mysql)$/) {
-	$dsn .= "dbname=$db_database";
-	if ($db_host) {
-	    $dsn .= ";host=$db_host";
-	}
-	if ($db_port) {
-	    $dsn .= ";port=$db_port";
-	}
-    }else{
-	print "Unknown DB type: $db_type\n";
-	return 0;
+    $dsn .= "dbname=$db_database";
+    if ($db_host) {
+	$dsn .= ";host=$db_host";
+    }
+    if ($db_port) {
+	$dsn .= ";port=$db_port";
     }
     
     print "DEBUG: init: $dsn\n" if $DEBUG;
     
-    if( my $dbh = DBI->connect( $dsn, $db_dba, $db_dba_password ) ) {
+    if( my $dbh = DBI->connect( $dsn, $db_dba, $db_dba_password, {RaiseError => 1} ) ) {
 	print "DEBUG: Connected successfully\n" if $DEBUG;
 	return $dbh;
     } else {
@@ -66,11 +61,32 @@ sub dbdisconnect {
 ##################################################
 sub processdata {
     my ($dbh, $lines) = @_;
-    my $rows;
-    while (my $cmd = shift @$lines ){
+    my $statements = &build_statements($lines);
+    my $rows = 0;
+    while (my $cmd = shift @$statements ){
 	$cmd  =~ /^(.*)$/;
+	chomp($cmd);
+	print "DEBUG: ($cmd): ";
 	$rows = $dbh->do( $cmd );
-	print "DEBUG: ($cmd): rows affected: $rows\n" if $DEBUG;
+	print "rows affected: $rows\n" if $DEBUG;
     }
     return 1;
+}
+
+##################################################
+# Convert lines into complete statements
+# $lines must be an arrayref
+sub build_statements{
+    my ($lines) = @_;
+    my @statements;
+    my $statement = "";
+    foreach my $line ( @$lines ) {
+	$statement .= $line;
+	if ($line =~ /;\s*$/) {
+	    $statement =~ s/;$//g;
+	    push @statements, $statement;
+	    $statement = "";
+	}
+    }
+    return \@statements;
 }
--- bin/updatedevices.pl
+++ bin/updatedevices.pl
@@ -1,248 +1,168 @@
 #!<<Make:PERL>>
-
+#
+# updatedevices.pl - Update Netdot Devices
+#
+# This script retrieves SNMP information from one ore more devices
+# and updates the Netdot database.  It can retrive basic device information
+# as well as bridge forwarding tables and ARP caches.
+#
 use lib "<<Make:LIB>>";
+use Netdot::Model::Device;
 use Getopt::Long qw(:config no_ignore_case bundling);
 use strict;
-use Netdot::DeviceManager;
-use Netdot::DNSManager;
-use NetAddr::IP;
+use Log::Log4perl::Level;
 
-# We construct this early only to be able to access
-# the config attribute, which is common to all 
-# Netdot libraries
-my $dns = Netdot::DNSManager->new();
+# Variables that will hold given values
+my ($host, $block, $db);
 
-my ($host, $subnet, $db, $dm);
-my $COMSTR          = "public";
-my $ADDSUBNETS      = 0;
-my $HELP            = 0;
-my $DEBUG           = 0;
-my $EMAIL           = 0;
-my $PRETEND         = 0;
-my $RETRIES         = $dns->{config}->{DEFAULT_SNMPRETRIES};
-my $TIMEOUT         = $dns->{config}->{DEFAULT_SNMPTIMEOUT};
-my $VERSION         = $dns->{config}->{DEFAULT_SNMPVERSION};
+# Default values
+my $communities     = Netdot->config->get('DEFAULT_SNMPCOMMUNITIES');
+my $commstrs        = join ", ", @$communities if defined $communities;
+my $retries         = Netdot->config->get('DEFAULT_SNMPRETRIES');
+my $timeout         = Netdot->config->get('DEFAULT_SNMPTIMEOUT');
+my $version         = Netdot->config->get('DEFAULT_SNMPVERSION');
 
-my $usage = <<EOF;
- usage: $0 [ -c, --community <string> ] [ -v, --version <1|2|3> ] 
-           [ -r, --retries <integer> ] [ -t, --timeout <secs> ]
-           [ -g, --debug ] [ -m, --send_mail ] [ -a, --add-subnets ] [ -p, --pretend ]
-           -H, --host <hostname|address> | -d, --db |  -s, --subnet <CIDR block>
+# Flags
+my ($ADDSUBNETS, $SUBSINHERIT, $BGPPEERS, $INFO, $FWT, $ARP, $PRETEND, $HELP, $_DEBUG, $EMAIL, $PRETEND);
+
+# This will be reflected in the history tables
+$ENV{REMOTE_USER}   = "netdot";
+
+my $USAGE = <<EOF;
+ usage: $0 [ optional args ]
+           -H, --host <hostname|address> | -d, --db |  -b, --block <address/prefix>
+           [-I, --info]  [-F, --fwt]  [-A, --arp]
            
     
-    -H, --host <hostname|address>  update given host only.
-    -s, --subnet <CIDR block>      specify a v4/v6 subnet to discover
-    -c, --community <string>       SNMP community string (default $COMSTR)
-    -r, --retries <integer >       SNMP retries (default $RETRIES)
-    -t, --timeout <secs>           SNMP timeout in seconds (default $TIMEOUT)
-    -v, --version <integer>        SNMP version [1|2|3] (default $VERSION)
-    -d, --db                       update only DB existing devices
-    -a, --add-subnets              when discovering routers, add subnets to database if they do not exist
-    -p, --pretend                  do not commit changes to the db
-    -h, --help                     print help (this message)
-    -g, --debug                    set syslog level to LOG_DEBUG and print to STDERR
-    -m, --send_mail                Send output via e-mail instead of to STDOUT
-                                   (Note: debugging output will be sent to STDERR always)
+    -H, --host <hostname|address>  Update given host only.
+    -b, --block <address/prefix>   Specify an IP block to discover
+    -d, --db                       Update only DB existing devices
+    -c, --community <string>       SNMP community string(s) (default: $commstrs)
+    -r, --retries <integer >       SNMP retries (default: $retries)
+    -t, --timeout <secs>           SNMP timeout in seconds (default: $timeout)
+    -v, --version <integer>        SNMP version [1|2|3] (default: $version)
+    -I, --info                     Get device info
+    -F, --fwt                      Get forwarding tables
+    -A, --arp                      Get ARP tables
+    -a, --add-subnets              When discovering routers, add subnets to database if they do not exist
+    -i, --subs_inherit             When adding subnets, have them inherit information from the Device
+    -B, --with-bgp-peers           When discovering routers, maintain their BGP Peers
+    -p, --pretend                  Do not commit changes to the database
+    -h, --help                     Print help (this message)
+    -g, --debug                    Set syslog level to LOG_DEBUG
+    -m, --send_mail                Send logging output via e-mail instead of to STDOUT
+
+Options override default settings from config file.
     
 EOF
     
-use vars qw ( $output );
-
 # handle cmdline args
 my $result = GetOptions( "H|host=s"          => \$host,
-			 "s|subnet=s"        => \$subnet,
-			 "c|community:s"     => \$COMSTR,
-			 "r|retries:s"       => \$RETRIES,
-			 "t|timeout:s"       => \$TIMEOUT,
-			 "v|version:s"       => \$VERSION,
+			 "b|block=s"         => \$block,
 			 "d|db"              => \$db,
+			 "c|communities:s"   => \$commstrs,
+			 "r|retries:s"       => \$retries,
+			 "t|timeout:s"       => \$timeout,
+			 "v|version:s"       => \$version,
+			 "I|info"            => \$INFO,
+			 "F|fwt"             => \$FWT,
+			 "A|arp"             => \$ARP,
 			 "a|add-subnets"     => \$ADDSUBNETS,
+			 "i|subs_inherit"    => \$SUBSINHERIT,
+			 "B|with-bgp-peers"  => \$BGPPEERS,
 			 "p|pretend"         => \$PRETEND,
 			 "h|help"            => \$HELP,
-			 "g|debug"           => \$DEBUG,
+			 "g|debug"           => \$_DEBUG,
 			 "m|send_mail"       => \$EMAIL);
 
-if( ! $result ) {
-    print $usage;
+if ( ! $result ) {
+    print $USAGE;
     die "Error: Problem with cmdline args\n";
 }
-if( $HELP ) {
-    print $usage;
+if ( $HELP ) {
+    print $USAGE;
     exit;
 }
-if ( ($host && $subnet) || ($host && $db) || ($subnet && $db) ){
-    print $usage;
+if ( ($host && $block) || ($host && $db) || ($block && $db) ){
+    print $USAGE;
     die "Error: arguments -H, -s and -d are mutually exclusive\n";
 }
-if ( $DEBUG ){
-    $dm = Netdot::DeviceManager->new(loglevel=>'LOG_DEBUG', foreground=>1);
+unless ( $INFO || $FWT || $ARP ){
+    print $USAGE;
+    die "Error: You need to specify at least one of -I, -F or -A\n";
+}
+
+# Re-read communities, in case we were passed new ones
+@$communities = split /,/, $commstrs if defined $commstrs;
+# Remove any spaces
+map { $_ =~ s/\s+// } @$communities;
+
+die "Cannot proceed without SNMP communities!\n" 
+    unless scalar @$communities;
+
+# Add a log appender depending on the output type requested
+my $logger = Netdot->log->get_logger('Netdot::Model::Device');
+my ($logstr, $logscr);
+if ( $EMAIL ){
+    $logstr = Netdot::Util::Log->new_appender('String', name=>'updatedevices.pl');
+    $logger->add_appender($logstr);
 }else{
-    $dm = Netdot::DeviceManager->new();
+    $logscr = Netdot::Util::Log->new_appender('Screen', stderr=>0);
+    $logger->add_appender($logscr);
 }
 
-if ( $PRETEND ){
-    # Tell the DB to not commit
-    # This should not affect the web interface because it uses a separate
-    # connection
-    $dm->db_auto_commit(0);
-    $output .= "Note: Executing with -p (pretend) flag.  Changes will not be committed to the DB\n";
+if ( $_DEBUG ){
+#   Set logging level to debug
+#   Notice that $DEBUG is imported from Log::Log4perl
+    $logger->level($DEBUG);
 }
 
-# This will be reflected in the history tables
-$ENV{REMOTE_USER} = "netdot";
+$logger->warn("Warning: Pretend (-p) flag enabled.  Changes will not be committed to the DB")
+    if ( $PRETEND );
 
+my $start = time;
+printf("Started at %s\n", scalar localtime($start));
+my %update_args = (communities  => $communities, 
+		   version      => $version,
+		   timeout      => $timeout,
+		   retries      => $retries,
+		   pretend      => $PRETEND,
+		   add_subnets  => $ADDSUBNETS,
+		   subs_inherit => $SUBSINHERIT,
+		   bgp_peers    => $BGPPEERS,
+		   );
 if ( $host ){
-    $output .= "Discovering single device: $host\n";
-    &discover({host=>$host, comstr=>$COMSTR});
-
-}elsif( $subnet ){
-    my $start = time;
-    $output .= sprintf ("Discovering all devices in subnet: $subnet\n");
-    $output .= sprintf ("Started at %s\n", scalar localtime($start));
-    my $net = NetAddr::IP->new($subnet);
-    # Make sure we work with the network address
-    $net = $net->network();
-    my %devices;
-    for (my $nip = $net+1; $nip < $nip->broadcast; $nip++){
-	# We want to get the device object before calling update_device
-	# in order to keep a list of checked devices
-	my $argv = {};
-	if ( $argv = $dm->find_dev($nip->addr) ){
-	    # We now have device and comstr in $argv
-	    my $device = $argv->{device};
-	    my $name = ($device->name) ? $device->name->name : $device->id;
-	    $argv->{host} = $nip->addr;
-	    # Make sure we don't query the same device more than once
-	    if (exists $devices{$device->id}){
-		printf ("Device %s already queried. Skipping\n", $name) if $DEBUG;
-		next;
-	    }
-	    $devices{$device->id} = '';
-	    unless ( $device->canautoupdate ){
-		printf ("Device %s was set to not auto-update. Skipping \n", $name) if $DEBUG;
-		next;
-	    }
-	}else{
-	    $argv->{device}  = 'NEW'; # Tell update_device to not look for a device object
-	    $argv->{host}    = $nip->addr;
-	    $argv->{comstr}  = $COMSTR;
-	}
-	&discover($argv);
+    $logger->info("Updating single device: $host");
+    my $dev;
+    if ( $INFO ){
+	$dev = Device->discover(host=>$host, %update_args);
     }
-    $output .= sprintf ("Total runtime: %s secs\n", (time-$start));
-
-}elsif( $db ){
-    $output .= sprintf ("Updating all devices in the DB\n");
-    my $start = time;
-    $output .= sprintf ("Started at %s\n", scalar localtime($start));
-    my $it = Device->retrieve_all;
-    while ( my $device = $it->next ) {
-	my $name = ($device->name) ? $device->name->name : $device->id;
-	unless ( $device->canautoupdate ){
-	    printf ("Device %s was set to not auto-update. Skipping \n", $name) if $DEBUG;
-	    next;
-	}
-	my $target;
-	# Try to use existing IP address
-	# Start with the one associated with the name
-	if ( $device->name && $device->name->arecords ){
-	    my $ar  = ($device->name->arecords)[0];
-	    $target = $ar->ipblock->address;
-	    # Or just grab any address
-	}elsif ( my @ips = $dm->getdevips($device) ){
-	    $target = $ips[0]->address;
-	    # Or use the name
-	}elsif ( $device->name && $device->name->name ){
-	    # Use FQDN
-	    $target = $device->name->name . "." . $device->name->zone->mname;
-	}
-	if ( $target ){
-	    &discover({host   => $target, 
-		     comstr => $device->community,
-		     device => $device });
-	}else{
-	    die "Could not determine target address or hostname";
-	}
+    if ( $FWT ){
+	$dev ||= Device->search(name=>$host)->first;
+	$dev->fwt_update();
     }
-    $output .= sprintf ("Total runtime: %s secs\n", (time-$start));
+    if ( $ARP ){
+	$dev ||= Device->search(name=>$host)->first;
+	$dev->arp_update();	
+    }
+}elsif ( $block ){
+    $logger->info("Updating all devices in block: $block");
+    if ( $INFO ){
+	my $r = Device->snmp_update_block(block=>$block, %update_args);
+    }
+}elsif ( $db ){
+    $logger->info("Updating all devices in the DB");
+    if ( $INFO ){
+	my $r = Device->snmp_update_all(%update_args);
+    }
 }else{
-    print $usage;
+    print $USAGE;
     die "Error: You need to specify one of -H, -s or -d\n";
 }
 
-if ( $output ){
-    if ( $EMAIL ){
-	if ( ! $dm->send_mail(subject=>"Netdot Device Updates", body=>$output) ){
-	    die "Problem sending mail: ", $dm->error;
-	}
-    }else{
-	print STDOUT $output;
-    }
-}
+$logger->info(sprintf("Total runtime: %s secs\n", (time - $start)));
 
-# Fetch SNMP info and update database
-sub discover {
-    my $argv = shift;
-    my $r;
-    $argv->{addsubnets} = $ADDSUBNETS;
-
-    # Get both name and IP for better error reporting
-    my ($name, $ip);
-    my $v4 = '(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})';
-    if ( $host =~ /$v4/ || $host =~ /:/ ){
-	# $host looks like an IP address
-	if ( $argv->{device} && $argv->{device}->name ){
-	    # use whatever is in the DB
-	    $name = $argv->{device}->name->name;
-	}else{
-	    # or resolve it
-	    $name = $dns->resolve_ip($argv->{host}) || "?";
-	}
-	$ip   = $argv->{host};
-    }else{
-	# $host looks like a name
-	$name = $argv->{host};
-	$ip   = ($dns->resolve_name($name))[0] || "?";
-    }
-
-    # Fetch SNMP info from device
-    # We do this first to avoid unnecessarily calling update_device
-    # (within a transaction) if host does not respond
-    unless ( $argv->{info} = $dm->get_dev_info(host     => $argv->{host}, 
-					       comstr   => $argv->{comstr},
-					       version  => $VERSION,
-					       timeout  => $TIMEOUT,
-					       retries  => $RETRIES) ){
-	# error should be set
-	my $err = sprintf("Error %s (%s): %s\n", $name, $ip, $dm->error);
-	$output .= $err;
-	print $err if $DEBUG;
-	return 0;
-    }
-
-    if ( $PRETEND ){
-	# AutoCommit has been turned off, so rollback each update
-	if ( $r = $dm->update_device($argv) ){
-	    $output .= sprintf ("%s", $dm->output);
-	    $dm->db_rollback;
-	}else{
-	    my $err = sprintf("Error: %s\n", $dm->error);
-	    $output .= $err;
-	    print $err if $DEBUG;
-	    $dm->db_rollback;
-	    return 0;
-	}
-    }else{
-	# Update device in database (atomically)
-	if ( $r = $dm->do_transaction( sub{ return $dm->update_device(@_) }, $argv) ) {
-	    $output .= sprintf ("%s", $dm->output);
-	}else{
-	    my $err  = sprintf("Error: %s\n", $dm->error);
-	    $output .= $err;
-	    print $err if $DEBUG;
-	    return 0;
-	}
-    }
-    return $r;
+if ( $EMAIL ){
+    Netdot::Util::Misc->send_mail(subject=>"Netdot Device Updates", body=>$logstr->string);
 }
 
