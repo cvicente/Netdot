cebf69e371ab08fbce47fe18cd87ac919e7953a4
Carlos Vincente <cvicente@uoregon.edu>
2008-03-04 19:21:30 +0000

    Extending prune_db.pl script to handle phyaddr, arpcache and fwtable tables in addition to history tables

--- bin/prune_db.pl
+++ bin/prune_db.pl
@@ -14,7 +14,8 @@ my $DEBUG       = 0;
 my $HELP        = 0;
 my $VERBOSE     = 0;
 my $EMAIL       = 0;
-my $NUM_MONTHS  = 12;
+my $TABLES;
+my $NUM_DAYS    = 365;
 my $NUM_HISTORY = 100;
 my $FROM        = Netdot->config->get('ADMINEMAIL');
 my $TO          = Netdot->config->get('NOCEMAIL');
@@ -22,110 +23,140 @@ my $SUBJECT     = 'Netdot DB Maintenance';
 my $output;
 
 my $usage = <<EOF;
- usage: $0 [ -m|--num_months <number> ] [ -n|--num_history <number> ] 
+ usage: $0   -T|--tables <regex>
+           [ -d|--num_months <number> ] [ -n|--num_history <number> ] 
            [ -v|--verbose ] [ -g|--debug ] 
-           [-e|--send_mail] [-f|--from] | [-t|--to] | [-S|--subject]
+           [ -m|--send_mail] [-f|--from] | [-t|--to] | [-s|--subject]
            
-    Deletes old items from the history tables as necessary.
+    Deletes old items from database as necessary.
 
-    What is considered "old"?
+    * For tables with corresponding "history" tables:
 
-    We want to keep NUM_MONTHS worth of information for every record. But
-    if a record doesn't change at all during the last NUM_MONTHS, we don't 
-    want to lose the history that does exist for it from before that time. 
-    To compromise, we will only check the history tables for old data 
-    if there are more than NUM_HISTORY items for a record.
+        We want to keep NUM_DAYS worth of history for every record. But
+       if a record doesn't change at all during the last NUM_DAYS, we don't 
+       want to lose the history that does exist for it from before that time. 
+       To compromise, we will only check the history tables for old data 
+       if there are more than NUM_HISTORY items for a record.  If there are 
+       more than NUM_HISTORY items for a record, then we drop anything older 
+       than NUM_DAYS.
+
+    * For tables with timestamps:
     
-    If there are more than NUM_HISTORY items for a record, then we drop 
-    anything older than NUM_MONTHS.
+        We delete records that are older than NUM_DAYS if there are fewer
+        than NUM_HISTORY items in that table.
 
            
-    -m, --num_months               Number of months worth of history items to keep (default: $NUM_MONTHS);
+    -T, --tables                   (Perl) Regular expression matching table names. Required.
+    -d, --num_days                 Number of days worth of items to keep (default: $NUM_DAYS);
     -n, --num_history              Number of history items to keep for each record (default: $NUM_HISTORY);
     -v, --verbose                  Print informational output
     -g, --debug                    Print (lots of) debugging output
-    -e, --send_mail                Send output via e-mail instead of to STDOUT
+    -m, --send_mail                Send output via e-mail instead of to STDOUT
     -f, --from                     e-mail From line (default: $FROM)
-    -S, --subject                  e-mail Subject line (default: $SUBJECT)
+    -s, --subject                  e-mail Subject line (default: $SUBJECT)
     -t, --to                       e-mail To line (default: $TO)
     
 EOF
     
 # handle cmdline args
-my $result = GetOptions( "m|num_months=i"  => \$NUM_MONTHS,
+my $result = GetOptions( "T|tables=s"      => \$TABLES,
+                         "d|num_days=i"    => \$NUM_DAYS,
 			 "n|num_history=i" => \$NUM_HISTORY,
-			 "e|send_mail"     => \$EMAIL,
+			 "m|send_mail"     => \$EMAIL,
 			 "f|from:s"        => \$FROM,
 			 "t|to:s"          => \$TO,
-			 "S|subject:s"     => \$SUBJECT,
+			 "s|subject:s"     => \$SUBJECT,
 			 "h|help"          => \$HELP,
 			 "v|verbose"       => \$VERBOSE,
 			 "g|debug"         => \$DEBUG,
 			 );
 
-if( ! $result ) {
+if ( ! $result || !$TABLES ) {
     print $usage;
     die "Error: Problem with cmdline args\n";
 }
-if( $HELP ) {
+if ( $HELP ) {
     print $usage;
     exit;
 }
 
+&debug(sprintf("%s: Executing with: T=\"%s\", d=%d, n=%s\n", $0, $TABLES, $NUM_DAYS, $NUM_HISTORY));
+
+my @tables;
+map { push @tables, $_ if ( $_->name =~ /$TABLES/i ) } Netdot->meta->get_tables(with_history=>1);
+
 # Get DB handle 
 my $dbh = Netdot::Model::db_Main();
 
-my @tables;
-map { push @tables, $_ if ($_ =~ /_history/ ) } sort $dbh->tables;
-map { $_ =~ s/\`//g } @tables;
-map { $_ =~ s/\_history//g } @tables;
-
-
-# date NUM_MONTHS ago
-my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime (time-($NUM_MONTHS*30*24*60*60)); # 30 days/month
+# date NUM_DAYS ago
+my ($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime (time-($NUM_DAYS*24*60*60));
 $year += 1900; $mon += 1;
-printf("NUM_MONTHS(%d) ago was : %d/%d\n", $NUM_MONTHS, $year, $mon) if $DEBUG;
 my $sqldate = sprintf("%4d-%02d-%02d %02d:%02d:%02d",$year,$mon,$mday,$hour,$min,$sec);
+&debug(sprintf("NUM_DAYS(%d) ago was : %s\n", $NUM_DAYS, $sqldate));
 
 my $start = time;
 
-foreach my $table (@tables) {
-    my $history_table    = $table."_history";
-    my $table_id_field   = lc($table)."_id";
-    my $total_deleted    = 0;
+foreach my $table ( @tables ) {
+    my $tablename      = lc($table->name);
+    my $is_history     = 1 if $table->is_history;
+    my $total_deleted  = 0;
+    my $r;
+
+    &debug(sprintf("Checking in %s \n", $tablename));
+
+    if ( $is_history ){
+	my $ot = $table->original_table;
+	next unless $ot;
+	my $table_id_field = lc($ot)."_id";
+
+	# for each unique table_id in the history table
+	my $q = $dbh->prepare("SELECT $table_id_field, COUNT(id) FROM $tablename GROUP BY $table_id_field");
+	$q->execute();
+	while (my ($table_id, $count) = $q->fetchrow_array()) {
+	    if ( $count > $NUM_HISTORY ) {
+		&debug(sprintf("%s record %s has %s history items\n", $tablename, $table_id, $count));
+		###################################
+		# Deletes history items that are older than NUM_DAYS.
+		# Note that this is run inside an 'if' statement ($count > $NUM_HISTORY), so
+		# we will only delete history items older than NUM_DAYS IF there are more
+		# than NUM_HISTORY history items.
+		
+		$r = $dbh->do("DELETE FROM $tablename WHERE $table_id_field=$table_id AND modified < '$sqldate'");
+	    }
+	}
+	################################################################################################
+	# Special cases
+    }elsif ( $tablename =~ /^arpcache$|^fwtable$|^physaddr$/ ){
+	
+	my $q = $dbh->prepare_cached("SELECT COUNT(id) FROM $tablename");
+	$q->execute();
+	while ( my $count = $q->fetchrow_array() ) {
+	    if ( $count > $NUM_HISTORY ) {
+		&debug(sprintf("%s has %s items\n", $tablename, $count));
+		
+		if ( $tablename eq 'physaddr' ){
+		    # "Static" means do not delete
+		    $r = $dbh->do("DELETE FROM $tablename WHERE static='0' AND last_seen < '$sqldate'");
+		}else{
+		    $r = $dbh->do("DELETE FROM $tablename WHERE tstamp < '$sqldate'");
+		}	    
+	    }
+	}
+    }	
+    if ( ($VERBOSE || $DEBUG) && $r > 0 ){
+	printf("%d rows deleted\n", $r);
+    }
+    $total_deleted += $r;
     
-    printf("Checking in %s \n", $history_table) if $DEBUG;
-
-    # for each unique table_id in the history table
-    my $q = $dbh->prepare("SELECT $table_id_field, COUNT(id) FROM $history_table GROUP BY $table_id_field");
-    $q->execute();
-    while (my ($table_id, $count) = $q->fetchrow_array()) {
-    	if ($count > $NUM_HISTORY) {
-            printf("%s record %s has %s history items\n", $history_table, $table_id, $count) if $DEBUG;
-
-            ###################################
-            # Deletes history items that are older than NUM_MONTHS.
-            # Note that this is run inside an 'if' statement ($count > $NUM_HISTORY), so
-            # we will only delete history items older than NUM_MONTHS IF there are more
-            # than NUM_HISTORY history items.
-
-            my $r = $dbh->do("DELETE FROM $history_table WHERE $table_id_field=$table_id AND modified < '$sqldate'");
-            if ( $r ){
-                if ( $VERBOSE ){
-                    printf("%d rows deleted\n", $r) if $DEBUG;
-                }
-                $total_deleted += $r;
-            }
-        }
-    }
     $output .= sprintf("A total of %d rows deleted from %s\n", 
-		       $total_deleted, $history_table) if (($VERBOSE || $DEBUG) && $total_deleted);    
-
-    if ($total_deleted > 0) {
-        # now optimize the table to free up the space from the deleted records
-        printf("Freeing deleted space in %s\n", $history_table) if $DEBUG;
-        $dbh->do("OPTIMIZE TABLE $history_table");
+		       $total_deleted, $tablename) if (($VERBOSE || $DEBUG) && $total_deleted);    
+    
+    if ( $total_deleted > 0 ) {
+	# now optimize the table to free up the space from the deleted records
+	printf("Freeing deleted space in %s\n", $tablename) if $DEBUG;
+	$dbh->do("OPTIMIZE TABLE $tablename");
     }
+    
 }
 
 my $end = time;
@@ -139,4 +170,9 @@ if ( $EMAIL ){
 		     body    => $output );
 }else{
     print STDOUT $output;
+}
+
+sub debug {
+    my $msg = shift @_;
+    print $msg if $DEBUG;
 }
