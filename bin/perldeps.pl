aa1e57608b9f14b6b0d3d6f31da4bca9f2680100
Carlos Vincente <cvicente@uoregon.edu>
2010-01-28 00:06:32 +0000

    Fixes in perl module installation script

--- bin/perldeps.pl
+++ bin/perldeps.pl
@@ -24,19 +24,21 @@ Arguments:
 #same idea as apt, but refers to the module name in the rpm repository.
 
 my @DEPS = (
+    {cpan=> 'Module::Build' , apt=> 'libmodule-build-perl', rpm=>'perl-Module-Build'},
     {cpan=> 'CGI 3.20' , apt=> '', rpm=>''},
     {cpan=>'Ima::DBI 0.35', apt=> 'libima-dbi-perl', rpm=>'perl-Ima-DBI'},
     {cpan=>'Class::DBI 3.0.10', apt=> 'libclass-dbi-perl', rpm=>'perl-Class-DBI'},
     {cpan=>'Class::DBI::AbstractSearch', apt=> 'libclass-dbi-abstractsearch-perl', rpm=> 'perl-Class-DBI-AbstractSearch'},
-    {cpan=>'Apache2::Request', apt=>'libapache2-request-perl', rpm=>''},
+    {cpan=>'Apache2::Request', apt=>'libapache2-request-perl', rpm=>'libapreq2 libapreq2-devel perl-libapreq2'},
     {cpan=>'HTML::Mason 1.31',apt=>'libhtml-mason-perl',rpm=>'perl-HTML-Mason'},
     {cpan=>'Apache::Session 1.6', apt=>'libapache-session-perl', rpm=>'perl-Apache-Session'},
     {cpan=>'URI::Escape', apt=>'liburi-perl', rpm=>'perl-URI'},
     {cpan=>'DBIx::DataSource', apt=> 'libdbix-datasource-perl', rpm=>''},
-    {cpan=>'GraphViz 2.02', apt=> 'graphviz', rpm=>''},
+    {cpan=>'GraphViz 2.02', apt=> 'graphviz', rpm=>'graphviz graphviz-devel perl-GraphViz'},
     {cpan=>'SQL::Translator 0.07', apt=>'libsql-translator-perl', rpm=>'perl-SQL-Translator'},
     {cpan=>'SNMP::Info 2.01', apt=>'libsnmp-info-perl', rpm=>'perl-SNMP-Info'},
     {cpan=>'NetAddr::IP', apt=>'libnetaddr-ip-perl', rpm=>'perl-NetAddr-IP'},
+    {cpan=>'Apache2::AuthCookie', apt=>'', rpm=>''},
     {cpan=>'Apache2::SiteControl 1.0', apt=>'', rpm=>''},
     {cpan=>'Log::Dispatch', apt=>'liblog-dispatch-perl', rpm=>'perl-Log-Dispatch'},
     {cpan=>'Log::Log4perl', apt=>'liblog-log4perl-perl', rpm=>'perl-Log-Log4perl'},
@@ -110,18 +112,18 @@ elsif ( $action eq 'install' || $action eq 'apt-get-install' || $action eq 'rpm-
 			elsif($results > 1){
 				print "Seems like there was a problem running your package management program, ";
 			}
-			print " would you like to continue and install all modules through CPAN [y/n]? [y] ";
-			$a = <STDIN>;
-			if($a =~ /(N|n)/){
-				exit(1);
-            		}
 		}
 	}		
-	#we call this regardless beacuse it checks to see if a module is installed 
-	#before trying anything, so lets just pass the whole hash in and if 
-	#something didn't take for whatever reason, hopefully it will be fixed by CPAN 
-	install_modules_cpan();
-    
+
+	print " would you like to continue and install all modules through CPAN [y/n]? [y] ";
+	$a = <STDIN>;
+	if($a =~ /(Y|y)/){
+	    #we call this regardless beacuse it checks to see if a module is installed 
+	    #before trying anything, so lets just pass the whole hash in and if 
+	    #something didn't take for whatever reason, hopefully it will be fixed by CPAN 
+	    install_modules_cpan();
+	}
+
 	#finally, lets call run_test to show the user if anything is missing
 	print "===============RESULTS===============\n";
 	run_test();
