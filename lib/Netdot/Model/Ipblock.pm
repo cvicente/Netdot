7c5d37459037b68637ba586dcff7999fb299d79b
Carlos Vincente <cvicente@uoregon.edu>
2010-02-17 06:30:51 +0000

    Bug fix in SQL query

--- lib/Netdot/Model/Ipblock.pm
+++ lib/Netdot/Model/Ipblock.pm
@@ -2348,12 +2348,11 @@ sub get_addresses_by {
     my $id = $self->id;
     my $query = "    
     SELECT    DISTINCT(ipblock.id)
-    FROM      ipblockstatus, rraddr, rr,ipblock 
+    FROM      ipblockstatus, ipblock 
+    LEFT JOIN (rraddr, rr) ON (rraddr.ipblock=ipblock.id AND rraddr.rr=rr.id)
     LEFT JOIN entity ON (ipblock.used_by=entity.id)
     WHERE     ipblock.parent=$id
       AND     ipblock.status=ipblockstatus.id
-      AND     rraddr.ipblock=ipblock.id
-      AND     rraddr.rr=rr.id
     ORDER BY  $sort2field{$sort}";
 
     my $dbh  = $self->db_Main();
