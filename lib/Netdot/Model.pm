35c4575ae2ef7a0933721dd62eb2f5c41942aef4
Carlos Vicente <cvicente@nsrc.org>
2011-07-13 22:54:31 -0700

    Add full error from DB for duplicate on update exception

--- lib/Netdot/Model.pm
+++ lib/Netdot/Model.pm
@@ -656,7 +656,7 @@ sub update {
 	# Class::DBI shows a full stack trace
 	# Try to make it less frightening for the user
 	if ( $e =~ /Duplicate/i ){
-	    $msg .= "Some values are duplicated";
+	    $msg .= "Some values are duplicated. Full error: $e";
 	}elsif ( $e =~ /invalid input syntax/i ){
 	    $msg .= "Some fields have invalid input syntax";
 	}elsif ( $e =~ /out of range/i ){
