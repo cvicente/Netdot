880d7bda6c3e418e5bd72f2cf4af460b467e6846
Carlos Vicente <cvicente@ns.uoregon.edu>
2011-08-25 08:48:41 -0700

    Ignore duplex status 'unknown' when finding duplex mismatches

--- lib/Netdot/Model/Interface.pm
+++ lib/Netdot/Model/Interface.pm
@@ -81,6 +81,8 @@ sub find_duplex_mismatches {
                                                  AND   r.oper_status='up'
                                                  AND   i.oper_duplex!=''
                                                  AND   r.oper_duplex!=''
+                                                 AND   i.oper_duplex!='unknown'
+                                                 AND   r.oper_duplex!='unknown'
                                                  AND   i.oper_duplex!=r.oper_duplex");
 
     if ( $mismatches ){
