8e04f04ef6398df987bda647011b60a8ee0ab698
Carlos Vicente <cvicente@ns.uoregon.edu>
2012-02-02 08:46:50 -0800

    Revert "Updated automatic topology discovery so it gets the communities out of Site.conf. This makes it much more usable"
    This reverts commit 90513537955aeb57c4ad904546cf77d038915ee2.

--- lib/Netdot/Topology.pm
+++ lib/Netdot/Topology.pm
@@ -562,10 +562,9 @@ sub get_dp_links {
 	    } 		
 	}
 	$logger->info("Topology::get_dp_links: Discovering unknown neighbor: $ip");
-    my $communities = Netdot->config->get('DEFAULT_SNMPCOMMUNITIES');
-    eval {
-        push @new_devs, Device->discover(name=>$ip, do_info=>1, communities=>$communities);
-    };
+	eval {
+	    push @new_devs, Device->discover(name=>$ip);
+	};
     }
     if ( @new_devs && $argv{recursive} ){
 	$logger->info("Netdot::Topology::get_dp_links: Recursively discovering unknown neighbors");
