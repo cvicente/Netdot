b019747700a9eba37d0866c776eab7b7fe577e69
Carlos Vicente <cvicente@cpan.org>
2012-05-31 09:39:06 -0400

    Added show all/hide all buttons to contact list page

--- htdocs/java_script/toggle.js
+++ htdocs/java_script/toggle.js
@@ -27,6 +27,39 @@ function toggleContact(contact_id) {
     toggleLayer(contact_id+'_buttons');
     document.getElementById(contact_id+'_link').innerHTML = (display=='none'?'[+]':'[-]');
 }
+function toggleAllContacts(container, tog) {
+    var e = document.getElementById(container).getElementsByClassName('contact');
+    
+    var i;
+    for (i = 0; i< e.length; i++) {
+	elem = e[i];
+	
+	if (tog == 0) { // hide
+	    elem.style.display = 'none';
+	    
+	    buttons = document.getElementById(elem.id + '_buttons');
+	    buttons.style.display = 'none';
+	    
+	    click = document.getElementById(elem.id+'_link').innerHTML = '[+]';
+	} else {
+	    elem.style.display = 'block';
+	    
+	    buttons = document.getElementById(elem.id + '_buttons');
+	    buttons.style.display = 'block';
+	    
+	    click = document.getElementById(elem.id+'_link').innerHTML = '[-]';
+	}
+    }
+    
+    e = document.getElementById(container+'_toggleAll');
+    if (e.innerHTML == '[show all]') {
+	e.innerHTML = '[hide all]';
+	e.href = "javascript:toggleAllContacts('"+ container +"', 0);";
+    } else {
+	e.innerHTML = '[show all]';
+	e.href = "javascript:toggleAllContacts('"+ container +"', 1);";
+    }
+}
 function emptyfunction() {
     // do nothing
     // this function is called by the jsrs code after toggleTasks is called.
